24a56eea04e33bde25a1caa58686bd81bf7168d9
asddsa <94194669+azsdwv@users.noreply.github.com>
2022-08-20 20:31:55 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,3 +6,16 @@ function init() {
 }
 
 ymaps.ready(init)
+
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
